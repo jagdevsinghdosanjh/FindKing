ed506a023afe1181234afe0ef53928e496492401
Jagdev Singh Dosanjh <jagdevsinghdosanjh@gmail.com>
2023-03-24 23:29:01 +0530

    Commit to Upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function getInputValue() {
+    // Selecting the input element and get its value 
+    let inputVal = document.getElementById("inputId").value;
+    // Displaying the value
+    alert(inputVal);
